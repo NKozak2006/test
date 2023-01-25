b2db0be3767baaf88e5eb7b900443d4139ed8c8b
NKozak2006 <123200571+NKozak2006@users.noreply.github.com>
2023-01-24 19:18:49 -0600

    Update index.php

--- index.php
+++ index.php
@@ -23,10 +23,8 @@
     include "NavBar.html";
 
     $conn = mysqli_init();
-    $conn->ssl_set("Nzc3YzQzYTZmYWFjNjc5YWZlODQyNTIxZWI3ZjI4MTdmNjU4MGEwOQ==", "https://www.cleardb.com/service/1.0/api", null, null, null);
+    $conn->ssl_set("Nzc3YzQzYTZmYWFjNjc5YWZlODQyNTIxZWI3ZjI4MTdmNjU4MGEwOQ==", "https://www.cleardb.com/service/1.0/api", null, null, null) OR die("<script>alert('died')</script>");
     $conn->real_connect("heroku_711a6f970c6f4f4", "b6536ab746a7ea", "4d6096b2");
-
-    echo "<script>alert('this works')</script>";
     
     $dbname = "Q和A";
 
